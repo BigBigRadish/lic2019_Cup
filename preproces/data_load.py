da15f5a8c73d93261936f47c8b7bf6498ec07f2b
BigBigRadish <luozhukun@163.com>
2019-03-13 21:59:05 +0800

    数据处理

--- preproces/data_load.py
+++ preproces/data_load.py
@@ -10,8 +10,12 @@ Jiangxi university of finance and economics
  fpath1, fpath2 分别是 原始输入文件 and 目标文件
 
 '''
+import collections 
+import pickle
 import tensorflow as tf
 from utils import calc_num_batches
+import codecs
+import numpy as np
 def load_vocab(vocab_fpath):
 
     '''Loads vocabulary file and returns idx<->token maps
@@ -21,11 +25,26 @@ def load_vocab(vocab_fpath):
     Returns
     two dictionaries.
     '''
-    vocab = [line.split()[0] for line in open(vocab_fpath, 'r').read().splitlines()]
+    with codecs.open(vocab_fpath,'rb') as f:
+        vocab=pickle.load(f)
     token2idx = {token: idx for idx, token in enumerate(vocab)}
     idx2token = {idx: token for idx, token in enumerate(vocab)}
+#     print(token2idx)
     return token2idx, idx2token
-
+def generate_vocab(input_file, vocab_file): 
+        with codecs.open(input_file, "rb",encoding='utf-8') as f: 
+            data = f.read().split() # 统计出每个字符出现多少次，统计结果总共有65个字符，所以vocab_size = 65
+            print(len(data)) 
+            counter = collections.Counter(data) # 按键值进行排序 
+            count_pairs = sorted(counter.items(), key=lambda x: -x[1]) # 得到所有的字符 
+            count_pairs=count_pairs
+            chars, _ = zip(*count_pairs) 
+            vocab = dict(zip(chars, range(1,len(chars)))) # 将字符写入文件 
+#             print(vocab)
+#             vocab_size = len(chars) # 得到字符的索引，这点在文本处理的时候是值得借鉴的 
+#             print(vocab_size)
+            with codecs.open(vocab_file, 'wb') as f1: 
+                pickle.dump(vocab, f1) # 使用map得到input文件中1115394个字符对应的索引 
 def load_data(fpath1, fpath2, maxlen1, maxlen2):
     '''Loads source and target data and filters out too lengthy samples.
     fpath1: source file path. string.
@@ -37,17 +56,18 @@ def load_data(fpath1, fpath2, maxlen1, maxlen2):
     sents2: list of target sents
     '''
     sents1, sents2 = [], []
-    with open(fpath1, 'r') as f1, open(fpath2, 'r') as f2:
-        for sent1, sent2 in zip(f1, f2):
-            print(sent1.strip())
+    with open(fpath1, 'r',encoding='utf-8') as f1, open(fpath2, 'r',encoding='utf-8') as f2:
+        for sent1, sent2 in zip(f1.readlines(), f2.readlines()):
+#             print(sent1.strip())
             if len(sent1.split()) + 1 > maxlen1: continue # 1: </s>
             if len(sent2.split()) + 1 > maxlen2: continue  # 1: </s>
-            sents1.append(sent1.strip())
-            sents2.append(sent2.strip())
+            sents1.append(sent1)
+            sents2.append(sent2)
+#     print(sents1)
     return sents1, sents2
 
 
-def encode(inp, type, dict):
+def encode(inp, type1, dict1):
     '''Converts string to number. Used for `generator_fn`.
     inp: 1维byte数组.
     type: "x" (source side) or "y" (target side)
@@ -55,10 +75,11 @@ def encode(inp, type, dict):
     Returns
     list of numbers
     '''
-    inp_str = inp.decode("utf-8")
-    if type=="x": tokens = inp_str.split() + ["</s>"]
-    else: tokens = ["<s>"] + inp_str.split() + ["</s>"]
-    x = [dict.get(t, dict["<unk>"]) for t in tokens]
+#     inp_str = inp.decode("utf-8")
+    tokens = inp.split()
+    print(tokens)
+    x = [dict1.get(t) for t in tokens]
+    print(x)
     return x
 
 def generator_fn(sents1, sents2, vocab_fpath):
@@ -77,13 +98,16 @@ def generator_fn(sents1, sents2, vocab_fpath):
         y_seqlen: int. sequence length of y
         sent2: str. target sentence
     '''
+#     print(sents1)
     token2idx, _ = load_vocab(vocab_fpath)
+    print(token2idx)
     for sent1, sent2 in zip(sents1, sents2):
+        print(sent1)
         x = encode(sent1, "x", token2idx)
         y = encode(sent2, "y", token2idx)
-        decoder_input, y = y[:-1], y[1:]
+#         decoder_input, y = y, y[1:]
         x_seqlen, y_seqlen = len(x), len(y)
-        yield (x, x_seqlen, sent1), (decoder_input, y, y_seqlen, sent2)
+        yield (np.array(x), x_seqlen), ( np.array(y), y_seqlen)#decoder_input,, sent1)
 
 
 def input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=False):
@@ -104,21 +128,26 @@ def input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=False):
         y_seqlen: int32 tensor. (N, )
         sents2: str tensor. (N,)
     '''
-    shapes = (([None], (), ()),
-              ([None], [None], (), ()))
-    types = ((tf.int32, tf.int32, tf.string),
-             (tf.int32, tf.int32, tf.int32, tf.string))
-    paddings = ((0, 0, ''),
-                (0, 0, 0, ''))
+    shapes = (([None],()),
+              ([None],()))
+    types = ((tf.int32, tf.int32), #, tf.string),
+             (tf.int32, tf.int32))#, tf.string))
+    paddings = ((-1, -1,),
+                (-1, -1,))
+    print(paddings)
     dataset = tf.data.Dataset.from_generator(
         generator_fn,
-        output_shapes=shapes,
         output_types=types,
-        args=(sents1, sents2, vocab_fpath))  # <- arguments for generator_fn. converted to np string arrays
-    if shuffle: # for training
-        dataset = dataset.shuffle(128*batch_size)
+#         (tf.int32, tf.int32),
+        output_shapes=shapes)
+#         (tf.TensorShape([None]), tf.TensorShape([])))  # <- arguments for generator_fn. converted to np string arrays
+#     print(dataset)
+#     if shuffle: # for training
+#         dataset = dataset.shuffle(128*batch_size)
+#     print(dataset)
     dataset = dataset.repeat()  # iterate forever
     dataset = dataset.padded_batch(batch_size, shapes, paddings).prefetch(1)
+    
     return dataset
 
 
@@ -139,11 +168,18 @@ def get_batch(fpath1, fpath2, maxlen1, maxlen2, vocab_fpath, batch_size, shuffle
     '''
     sents1, sents2 = load_data(fpath1, fpath2, maxlen1, maxlen2)
     batches = input_fn(sents1, sents2, vocab_fpath, batch_size, shuffle=shuffle)
+    print(batches)
     num_batches = calc_num_batches(len(sents1), batch_size)
     return batches, num_batches, len(sents1)
-if __name__=='main':
+if __name__ == '__main__':
     fpath='../data/train_sentence.txt'
-    load_data(fpath, fpath,100, 100)
-    with open(fpath,'r') as f:
-            print(f)
-    
+    train_pos_path='../data/train_pos.txt'#train pos
+    vocab_file='../data/vocab.pkl'
+    pos_vacab_file='../data/pos_vocab.pkl'
+    train_entity_file='../data/train_entity.txt'
+#     generate_vocab(train_pos_path, pos_vacab_file)
+#     with codecs.open(pos_vacab_file, 'rb') as f1: 
+#                 vocab=pickle.load(f1) # 使用map得到input文件中1115394个字符对应的索引
+#     print(vocab)
+    get_batch(fpath, train_entity_file, 50, 50, vocab_file, 50, shuffle=False)
+#     load_vocab(vocab_file)
